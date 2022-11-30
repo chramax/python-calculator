e882bffbeb3c9210ad424456ee43e0fc6dc98093
Jayson <88486804+iamjaysondt@users.noreply.github.com>
2022-11-30 08:39:21 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,17 @@
+val1  = 0;
+val1  = 0;
+opp = ',';
+output = 0;
+
+val1 = input ('Input value 1: ')
+opp = input ('Input opperator: ')
+val2 = input ('Input value 2: ')
+
+if opp == '+':
+    print(int(val1)+int(val2))
+if opp == '-':
+    print(int(val1)-int(val2))
+if opp == '*':
+    print(int(val1)*int(val2))
+if opp == '/':
+    print(int(val1)/int(val2))
